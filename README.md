ba382b3a891c696cddba9be05e06df68fb230d5a
Rohit Kaul <rkaul@newrelic.com>
2021-03-03 12:16:18 -0800

    Address "missing data" in Checks API, Add README, cleanup serverless.yml, add config file

--- README.md
+++ README.md
@@ -1,39 +1,47 @@
 [![New Relic Experimental header](https://github.com/newrelic/opensource-website/raw/master/src/images/categories/Experimental.png)](https://opensource.newrelic.com/oss-category/#new-relic-experimental)
 
-# [New Relic connector for Trend Micro Cloud One Conformity]
+# New Relic connector for Trend Micro Cloud One Conformity
 
->The project integrates [Trend Micro Cloud One Conformity](https://www.trendmicro.com/en_us/business/products/hybrid-cloud/cloud-one-conformity.html) with [New Relic](https://newrelic.com/) when using the [AWS integration](https://www.cloudconformity.com/help/conformity-bot/aws-integration.html). The solution relies on [Amazon SNS](https://www.cloudconformity.com/help/communication/communication-channels/amazon-sns-communication.html) integration offered by Conformity. With this integration, you will complement your AWS observability by adding in Conformity monitoring, now using your New Relic account.
-
-
->[Brief description - what is the project and value does it provide? How often should users expect to get releases? How is versioning set up? Where does this project want to go?]
+>The project integrates [Trend Micro Cloud One Conformity](https://www.trendmicro.com/en_us/business/products/hybrid-cloud/cloud-one-conformity.html) ("Conformity") with [New Relic](https://newrelic.com/) when using the [AWS integration](https://www.cloudconformity.com/help/conformity-bot/aws-integration.html). The "solution" relies on [Amazon SNS](https://www.cloudconformity.com/help/communication/communication-channels/amazon-sns-communication.html) integration offered by Conformity. With this integration, you will complement your AWS observability by adding in Conformity monitoring, now using your New Relic account alongside all the telemetry for all your applications and services.
 
 ## Installation
 
-> [Include a step-by-step procedure on how to get your code installed. Be sure to include any third-party dependencies that need to be installed separately]
+> Make sure you have access to the following:
+> * AWS account where you can deploy this solution.
+> * Trend Micro Cloud One Conformity account that is linked with your AWS account.
+> * New Relic account that is linked with your AWS account. If you don’t already have a New Relic account, you can sign up for a free account in the [AWS Marketplace](https://aws.amazon.com/marketplace/pp/B08L5FQMTG).
+
+> Deploy [Conformity-to-S3](https://github.com/raphabot/Conformity-to-S3) solution to your AWS account before you deploy this solution. The quickest way to do so is by using this AWS CloudFormation [template](https://github.com/raphabot/Conformity-to-S3/releases/latest/download/ConformityToS3Stack.template.json).
+
+> Be sure to add the necessary information to the  configuration file `config.dev.yml` in the project root directory. Replace the placeholders (in chevrons) with actual values. If you choose to fork this repository, do not commit this file as it holds sensitive information about your Trend Micro and New Relic accounts.
+
+> Make sure you've set up [AWS Command Line Interface](https://aws.amazon.com/cli/) (AWS CLI) in your machine or build host from where you plan to deploy this solution.
+
+> Make sure you've installed the latest version of `serverless` CLI.
+
+> You need to have [Docker](https://docs.docker.com/install/) installed, along with [Python 3](https://www.python.org/downloads/).
+
+> Initialize the node modules by running this command from the project root directory:
+ `npm install`
+
+> Deploy the solution using [serverless](https://www.serverless.com/framework/docs/getting-started/) CLI by running the following command. If you have [AWS CLI profile](https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-profiles.html) set up, include `–-profile <Profile Name>`.
+
+> `sls deploy`
+
+> This deploys the solution using the configuration defined in `config.dev.yml` file in the `us-east-1` Region of your AWS account, by default. You can specify the AWS account and Region by setting up your AWS CLI Profile on your machine or build host. You must deploy this solution in the same account and Region where you deployed the "Conformity-to-S3" solution.
 
 ## Getting Started
->[Simple steps to start working with the software similar to a "Hello World"]
+Refer to this [architecture diagram](architecture.png) for this solution's deployment architecture in your AWS environment.
+Once the solution is deployed to your AWS account, you should start to see the custom event named `TMCloudOneConformityEvent` reported into your New Relic Dashboards and Data Explorer.
 
-## Usage
->[**Optional** - Include more thorough instructions on how to use the software. This section might not be needed if the Getting Started section is enough. Remove this section if it's not needed.]
-
-
-## Building
-
->[**Optional** - Include this section if users will need to follow specific instructions to build the software from source. Be sure to include any third party build dependencies that need to be installed separately. Remove this section if it's not needed.]
-
-## Testing
-
->[**Optional** - Include instructions on how to run tests if we include tests with the codebase. Remove this section if it's not needed.]
+You can then build a [dashboard](new-relic-dashboard.png) in your New Relic account to keep tabs on your AWS account checks reported by Conformity.
 
 ## Support
 
-New Relic hosts and moderates an online forum where customers can interact with New Relic employees as well as other customers to get help and share best practices. Like all official New Relic open source projects, there's a related Community topic in the New Relic Explorers Hub. You can find this project's topic/threads here:
-
->Add the url for the support thread here
+New Relic hosts and moderates an online forum where customers can interact with New Relic employees as well as other customers to get help and share best practices.
 
 ## Contributing
-We encourage your contributions to improve [project name]! Keep in mind when you submit your pull request, you'll need to sign the CLA via the click-through using CLA-Assistant. You only have to sign the CLA one time per project.
+We encourage your contributions to improve New Relic connector for Trend Micro Cloud One Conformity! Keep in mind when you submit your pull request, you'll need to sign the CLA via the click-through using CLA-Assistant. You only have to sign the CLA one time per project.
 If you have any questions, or to execute our corporate CLA, required if your contribution is on behalf of a company,  please drop us an email at opensource@newrelic.com.
 
 **A note about vulnerabilities**
@@ -43,5 +51,4 @@ As noted in our [security policy](../../security/policy), New Relic is committed
 If you believe you have found a security vulnerability in this project or any of New Relic's products or websites, we welcome and greatly appreciate you reporting it to New Relic through [HackerOne](https://hackerone.com/newrelic).
 
 ## License
-[Project Name] is licensed under the [Apache 2.0](http://apache.org/licenses/LICENSE-2.0.txt) License.
->[If applicable: The [project name] also uses source code from third-party libraries. You can find full details on which libraries are used and the terms under which they are licensed in the third-party notices document.]
+New Relic connector for Trend Micro Cloud One Conformity is licensed under the [Apache 2.0](http://apache.org/licenses/LICENSE-2.0.txt) License.
--- handler.py
+++ handler.py
@@ -17,7 +17,7 @@ def toCamelCase(kebabCasedStr):
 def keysToCamelCase(dict0):
   return {toCamelCase(k): v for k, v in dict0.items()}
 
-def buildNREvent(ccEvent):
+def buildNREvent(ccEvent, accountIdMap=None, ruleIdCompliancesMap=None):
   from newrelic_telemetry_sdk import Event
   
   # flatten 'attributes' nested object (found in Checks API response)
@@ -33,8 +33,17 @@ def buildNREvent(ccEvent):
     ccEvent.update({'ruleId': ruleId})
   # accountId is a reserved word in NR Events API
   ccEvent.update({'ccAccountId': accountId})
+  compliances = ccEvent.get('compliances', None)
+  if compliances is None:
+    ccEvent.update({'compliances': ruleIdCompliancesMap.get(ruleId)})
+  service = ccEvent.get('service', None)
+  if service is None:
+    ccEvent.update({'service': ccEvent['id'].split(':')[3]})
+  cloudProviderId = ccEvent.get('cloudProviderId', None)
+  if cloudProviderId is None:
+    ccEvent.update({'cloudProviderId': accountIdMap.get(accountId)})
   # drop unwanted attributes and nested objects since NR Events API ignores them anyway
-  drop = {'accountId', 'attributes', 'relationships', 'type', 'extradata', 'extradataHash'}
+  drop = {'accountId', 'attributes', 'relationships', 'type', 'extradata', 'extradataHash', 'resolutionPageURL'}
   ccEvent = {k: v for k, v in ccEvent.items() if k not in drop}
   # convert kebab-cased keys to camel case (found in Checks API response)
   ccEvent = keysToCamelCase(ccEvent)
@@ -48,55 +57,98 @@ def buildNREvent(ccEvent):
   )
   return nrEvent
 
-def handleScheduledEvent(event):
+def makeCCRequest(url, params=''):
   import os
-  from newrelic_telemetry_sdk import Event, EventClient
-  import math
   import requests
   CC_REGION = 'us-west-2'
-  CC_ACCOUNTIDS = 'BVyTCW0Va'
-  CC_APIKEY = os.environ["CLOUD_CONFORMITY_API_KEY"]
-  CC_PAGE_SIZE = 1000
-  CC_REGIONS = ['global', 'us-east-1', 'us-east-2', 'us-west-1', 'us-west-2', 'ap-south-1', 'ap-northeast-2',
-'ap-southeast-1', 'ap-southeast-2', 'ap-northeast-1', 'ap-east-1', 'ca-central-1', 'eu-central-1', 'eu-west-1',
-'eu-west-2', 'eu-west-3', 'eu-north-1', 'eu-south-1', 'me-south-1', 'sa-east-1', 'af-south-1']
+  CC_APIKEY = os.environ['CLOUD_CONFORMITY_API_KEY']
   url = (
       "https://"
       + CC_REGION
-      + "-api.cloudconformity.com/v1/checks"
-      
+      + "-api.cloudconformity.com/v1/"
+      + url
   )
-  print('hello')
-  print('url', url)
   headers = {
       "Content-Type": "application/vnd.api+json",
       "Authorization": "ApiKey " + CC_APIKEY,
   }
+  response = requests.get(url, headers=headers, params=params)
+  response.raise_for_status()
+  response_json = response.json()
+  return response_json
+
+def makeCCPublicRequest(url, params=''):
+  import requests
+  CC_REGION = 'us-west-2'
+  url = (
+      "https://"
+      + CC_REGION
+      + ".cloudconformity.com/v1/"
+      + url
+  )
+  headers = {
+      "Content-Type": "application/json"
+  }
+  response = requests.get(url, headers=headers, params=params)
+  response.raise_for_status()
+  response_json = response.json()
+  return response_json
+
+# handle event from the schedular
+def handleScheduledEvent(event):
+  import os
+  from newrelic_telemetry_sdk import Event, EventClient
+  import math
+
+  # Fetch ruleId to compliances mapping using public "v1/services" endpoint
+  url = 'services'
+  response = makeCCPublicRequest(url)
+  rules = response.get('included')
+  awsRules = list(filter(lambda x: (x['type'] == 'rules' and x['attributes']['provider'] == 'aws'), rules))
+  ruleIdCompliancesMap = {x['id']: x['attributes']['compliances'] for x in awsRules}
+
+  # Fetch conformity account id to provider account id mapping using "v1/accounts" endpoint
+  url = 'accounts'
+  response = makeCCRequest(url)
+  response_json = response.get('data')
+  accountIdMap = {x['id']: x['attributes']['awsaccount-id'] for x in response_json}
+  
+  # Fetch all conformity checks for the account
+  CC_ACCOUNTIDS = os.environ['CLOUD_CONFORMITY_ACCOUNT_ID']
+  CC_PAGE_SIZE = 1000
+  CC_REGIONS = ['global', 'us-east-1', 'us-east-2', 'us-west-1', 'us-west-2', 'ap-south-1', 'ap-northeast-2',
+'ap-southeast-1', 'ap-southeast-2', 'ap-northeast-1', 'ap-east-1', 'ca-central-1', 'eu-central-1', 'eu-west-1',
+'eu-west-2', 'eu-west-3', 'eu-north-1', 'eu-south-1', 'me-south-1', 'sa-east-1', 'af-south-1']
+  url = 'checks'
   checks = []
+  # Fetch checks by region, to get around the API limits
+  # @see https://cloudone.trendmicro.com/docs/conformity/api-reference/#tag/Checks
+  # There is a 10k limit to the maximum number of overall results that can be returned. 
+  # Paging will not work for higher than this limit. 
+  # To fetch larger numbers, segment your requests using account and region filtering. 
+  # On larger accounts, filter requests per account, per region, per service.
   for region in CC_REGIONS:
-    response = requests.get(url, headers=headers, params='accountIds='+ CC_ACCOUNTIDS + '&filter[regions]=' + region + '&page[size]=' + str(CC_PAGE_SIZE))
-    
-    response.raise_for_status()
-    response_json = response.json().get('data')
+    response = makeCCRequest(url, params='accountIds='+ CC_ACCOUNTIDS + '&filter[regions]=' + region + '&page[size]=' + str(CC_PAGE_SIZE))
+    response_json = response.get('data')
     checks.extend(response_json)
-    total_records = response.json().get('meta').get('total')
+    total_records = response.get('meta').get('total')
     total_pages = math.ceil(total_records/CC_PAGE_SIZE)
     print('region', region)
     print('total records', total_records)
     print('total pages', total_pages)
     for page in range(1, total_pages):
-      res = requests.get(url, headers=headers, params='accountIds='+ CC_ACCOUNTIDS + '&filter[regions]=' + region + '&page[size]=' + str(CC_PAGE_SIZE) + '&page[number]=' + str(page))
-      res.raise_for_status()
-      res_json = res.json().get('data')
+      res = makeCCRequest(url, params='accountIds='+ CC_ACCOUNTIDS + '&filter[regions]=' + region + '&page[size]=' + str(CC_PAGE_SIZE) + '&page[number]=' + str(page))
+      res_json = res.get('data')
       checks.extend(res_json)
     print('checks so far', len(checks))
+
   data = json.dumps(checks)
   print('response', len(checks), data)
-  event_client = EventClient(os.environ["NEW_RELIC_INSERT_KEY"])
-  # convert to NR events
-  nrEvents = list(map(lambda c: buildNREvent(c), checks))
+  # Transform checks into New Relic Telemetry SDK event payload
+  nrEvents = list(map(lambda c: buildNREvent(c, accountIdMap, ruleIdCompliancesMap), checks))
   print('NR Events to be sent', json.dumps(nrEvents))
-  # batch events
+  # Batch events and send to New Relic
+  event_client = EventClient(os.environ['NEW_RELIC_INSERT_KEY'])
   chunkSize = 1000
   chunks = [nrEvents[i:i + chunkSize] for i in range(0, len(nrEvents), chunkSize)]
   for idx, chunk in enumerate(chunks):
@@ -114,14 +166,14 @@ def handler(event, context):
     print("Received event: " + json.dumps(event, indent=2))
 
     # check if it is a scheduled event from EventBridge
-    if event.get('source') == 'aws.events':
+    if event.get('source') == 'aws.events' and event.get('detail-type') == 'Scheduled Event':
       nrEvents = handleScheduledEvent(event)
       print('sent all {} events!'.format(len(nrEvents)))
       return {
           "message": "{} Events posted to your New Relic account successfully!".format(len(nrEvents))
       }
     else:
-      # Get the object from the event and show its content type
+      # Get the object (conformity check) from the S3 event
       bucket = event['Records'][0]['s3']['bucket']['name']
       key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
       try:
@@ -129,19 +181,17 @@ def handler(event, context):
           body = res['Body'].read().decode('utf-8')
       except Exception as e:
           print(e)
-          print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
+          print('Error getting object {} from bucket {}.'.format(key, bucket))
           raise e
-      # build custom event object
-      ccEvent = json.loads(body)[0]  # CC event objects are always an array of size 1
+      # Transform check into New Relic Telemetry SDK event payload
+      # conformity check objects are always an array of size 1
+      ccEvent = json.loads(body)[0]  
       nrEvent = buildNREvent(ccEvent)
       print('Sending NR event', nrEvent)
-      event_client = EventClient(os.environ["NEW_RELIC_INSERT_KEY"])
+      event_client = EventClient(os.environ['NEW_RELIC_INSERT_KEY'])
       response = event_client.send(nrEvent)
       response.raise_for_status()
       print("Event sent successfully!", nrEvent)
-
-      # Use this code if you don't use the http event with the LAMBDA-PROXY
-      # integration
       
       return {
           "message": "Event posted to your New Relic account successfully!",
